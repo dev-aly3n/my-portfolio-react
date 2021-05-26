fcdac3d065720ec6642271a7d2af7d8b91eab717
aly3n <aly3n.dev@gmail.com>
2021-05-26 22:25:59 +0430

    the project started. I just copied the last project files and remove all the extra  stuff

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function App() {
+ 
+  //// jsx
+  return (
+    <div className="app">
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    purge: [
+      'src/**/*.js',
+      'src/**/*.jsx',
+      'src/**/*.ts',
+      'src/**/*.tsx',
+      'public/**/*.html',
+    ],
+    theme: {
+      extend: {
+      },
+    },
+    variants: {
+      extend: {
+        backgroundColor: ['active'],
+        textColor: ['active'],
+      },
+    },
+    plugins: [],
+  }
